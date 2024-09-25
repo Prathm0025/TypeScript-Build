18d633b70df6c92934223e745d4a575d5498b5cb
Prathm0025 <prathamesh@underpinservices.com>
2024-09-25 16:12:28 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 // src/index.ts
 const greet = (name: string): string => {
-    return `Hello, ${name}! Welcome to the TypeScript project.`;
+    return `Hello, ${name}! Welcome to the TypeScript demo project.`;
 };
 
 console.log(greet("Prathamesh"));
